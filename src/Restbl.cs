7f330dab0ef65a91a8c1ff27b6cf365ab7defba0
ArchLeaders <archparliament@gmail.com>
2023-05-11 09:39:04 -0700

    Wrap native extension methods (C#)

---
+++ src/Restbl.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32.SafeHandles;
+using Native.IO.Handles;
+using System.Runtime.InteropServices;
+
+namespace CsRestbl;
+
+public unsafe partial class Restbl : SafeHandleZeroOrMinusOneIsInvalid
+{
+    [LibraryImport("cs_restbl")]
+    private static partial Restbl FromBinary(byte* src, int src_len);
+
+    [LibraryImport("cs_restbl")]
+    private static partial DataMarshal ToBinary(Restbl handle);
+
+    [LibraryImport("cs_restbl")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool Free(IntPtr handle);
+
+    public Restbl() : base(true) { }
+
+    public static Restbl FromBinary(ReadOnlySpan<byte> data)
+    {
+        fixed (byte* ptr = data) {
+            return FromBinary(ptr, data.Length);
+        }
+    }
+
+    public DataMarshal ToBinary()
+    {
+        return ToBinary(this);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        return Free(handle);
+    }
+}
