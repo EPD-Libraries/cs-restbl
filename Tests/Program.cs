d324c5981170d7cab8cc1c7e5ece184ad607dec1
ArchLeaders <archparliament@gmail.com>
2023-05-11 02:39:17 -0700

    Update tests

---
+++ Tests/Program.cs
@@ -0,0 +1,8 @@
+﻿using CsRestbl;
+using Native.IO.Services;
+
+NativeLibraryManager.RegisterAssembly(typeof(Program).Assembly, out bool isCommonLoaded)
+    .Register(RestblLibrary.Shared, out bool isRestblLoaded);
+
+Console.WriteLine($"Common Loaded: {isCommonLoaded}");
+Console.WriteLine($"Restbl Loaded: {isRestblLoaded}");
