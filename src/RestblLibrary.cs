55265eff400b57aa40bd3e55f4dd6df3e2a7ec4b
ArchLeaders <archparliament@gmail.com>
2023-05-11 02:38:13 -0700

    Impl NativeLibrary{T} wrapper

---
+++ src/RestblLibrary.cs
@@ -0,0 +1,9 @@
+﻿using Native.IO;
+using Native.IO.Services;
+
+namespace CsRestbl;
+
+public class RestblLibrary : NativeLibrary<RestblLibrary>, INativeLibrary
+{
+    protected override string Name { get; } = "cs_restbl";
+}
